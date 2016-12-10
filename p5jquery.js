adf8f4cb3d02b7a6d7a835e2d0be9b9fe11db4b8
Dmitry Isaev <x@x128.ru>
2016-12-10 13:21:01 +0300

    Initial commit, iOS version 0.001

---
+++ p5jquery.js
@@ -0,0 +1,27 @@
+function initPage() {
+  var div1 = $('<div data-role="page" class="ui-page ui-page-theme-a ui-page-active">')
+  var div2 = $('<div data-role="content" data-theme="a" style="margin: 10px;"></div>')
+  $('body').append(div1);
+  div1.append(div2);
+  return div2;
+}
+
+function addButton(parent, name) {
+  var btn = $('<a data-theme="a">' + name + "</a>");
+  parent.append(btn);
+  btn.button();
+  return btn;
+}
+
+function addText(parent, name) {
+  var div = $("<div>" + name + "</div>");
+  parent.append(div);
+  return div;
+}
+
+function addSlider(parent, min, max, value) {
+  var btn = $('<input type="range" value="' + value + '" min="' + min + '" max="' + max + '" data-theme="a" />');
+  parent.append(btn);
+  $("div").trigger('create');
+  return btn;
+}
---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  var page = initPage();
+
+  var btn1 = addButton(page, "say something");
+  var btn2 = addButton(page, "remove txt");
+
+  var txt1 = addText(page, 'hi');
+
+  btn1.click(function() {
+    txt1.text('In geometry, a digon is a polygon with two sides (edges) and two vertices. Its construction is degenerate in a Euclidean plane because the two sides would either coincide or one or both would have to be curved. A regular digon has both angles equal and both sides equal and is represented by Schläfli symbol {2}. It may be constructed on a sphere as a pair of 180 degree arcs connecting antipodal points, when it forms a lune. The digon is the simplest abstract polytope of rank 2. A truncated digon, t{2} is a square, {4}. An alternated digon, h{2} is a monogon, {1}.');
+  });
+  
+  btn2.click(function() {
+    txt1.remove();
+  });
+  
+  addSlider(page, 0, 100, 25);
+}
