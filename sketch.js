2e566b526a6179154669cd4ff69f75bc9de89942
Dmitry Isaev <x@x128.ru>
2016-12-10 22:44:02 +0300

    Button clickable area bugfix

--- sketch.js
+++ sketch.js
@@ -6,11 +6,11 @@ function setup() {
 
   var txt1 = addText(page, 'hi');
 
-  btn1.click(function() {
+  btn1.parent().click(function() {
     txt1.text('In geometry, a digon is a polygon with two sides (edges) and two vertices. Its construction is degenerate in a Euclidean plane because the two sides would either coincide or one or both would have to be curved. A regular digon has both angles equal and both sides equal and is represented by Schläfli symbol {2}. It may be constructed on a sphere as a pair of 180 degree arcs connecting antipodal points, when it forms a lune. The digon is the simplest abstract polytope of rank 2. A truncated digon, t{2} is a square, {4}. An alternated digon, h{2} is a monogon, {1}.');
   });
   
-  btn2.click(function() {
+  btn2.parent().click(function() {
     hideButton(btn1);
     txt1.text('slider value is ' + sliderValue('sl'));
   });
