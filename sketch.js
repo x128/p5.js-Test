d464adce56c3eec72357ae45936aeddbb593d117
Dmitry Isaev <x@x128.ru>
2016-12-25 19:26:50 +0300

    Stopwatch (released on Dec 23rd)

--- sketch.js
+++ sketch.js
@@ -3,19 +3,33 @@ function setup() {
 
   $('body').css('background-color', 'white');
 
-  var btn1 = addButton(page, "say something");
-  var btn2 = addButton(page, "hide btn1");
+  var btnStart = addButton(page, "start");
+  var btnStop = addButton(page, "stop");
+  var txtTime = addText(page, 'hi');
 
-  var txt1 = addText(page, 'hi');
+  var stopwatchRunning = false;
+  var stopwatchValue = 0;
+  
+  btnStart.parent().click(function() {
+    
+    if (stopwatchRunning) return;
+    stopwatchRunning = true;
 
-  btn1.parent().click(function() {
-    txt1.text('In geometry, a digon is a polygon with two sides (edges) and two vertices. Its construction is degenerate in a Euclidean plane because the two sides would either coincide or one or both would have to be curved. A regular digon has both angles equal and both sides equal and is represented by Schläfli symbol {2}. It may be constructed on a sphere as a pair of 180 degree arcs connecting antipodal points, when it forms a lune. The digon is the simplest abstract polytope of rank 2. A truncated digon, t{2} is a square, {4}. An alternated digon, h{2} is a monogon, {1}.');
+    stopwatchValue = 0;
+
+    var updateStopwatch = function() {
+      if (stopwatchRunning) {
+        stopwatchValue++;
+        txtTime.text(stopwatchValue);
+        setTimeout(updateStopwatch, 100);
+      }
+    };
+    updateStopwatch();
   });
   
-  btn2.parent().click(function() {
-    hideButton(btn1);
-    txt1.text('slider value is ' + sliderValue('sl'));
-    $('body').css('background-color', 'cyan');
+  btnStop.parent().click(function() {
+    if (!stopwatchRunning) return;
+    stopwatchRunning = false;
   });
   
   var slider = addSlider(page, 'sl', 0, 100, 25);
