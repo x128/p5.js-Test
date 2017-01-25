21107249135a0d34befcf8f1b6c046ddb3ca400c
Dmitry Isaev <x@x128.ru>
2017-01-25 10:39:39 +0300

    Flow transition example with dark page BG

--- Phone.js
+++ Phone.js
@@ -8,12 +8,12 @@ class Phone {
 
         var page1 = addPage('first');
         addText(page1, 'First page');
-        addPageTransitionButton(page1, 'goto 2', 'second', { transition : 'pop' });
+        addPageTransitionButton(page1, 'goto 2', 'second', { transition : 'flow' });
         new HomeButton(page1); // FIXME: non-reusable shit?
 
         var page2 = addPage('second');
         addText(page2, 'Second page');
-        addPageTransitionButton(page2, 'back to 1', 'first', { transition : 'pop', reverse : true });
+        addPageTransitionButton(page2, 'back to 1', 'first', { transition : 'flow', reverse : true });
         new Menu(page2);
         new HomeButton(page2); // FIXME: non-reusable shit?
 
